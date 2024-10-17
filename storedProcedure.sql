3fefdb95f29536069fdda14442e33c393e6156b8
$@#il <sahilsadekar249775@gmail.com>
2024-10-17 05:52:07 +0530

    pocedure revisit

--- storedProcedure.sql
+++ storedProcedure.sql
@@ -16,3 +16,58 @@ begin
 
 end myProcedure;
 /
+
+//demonstration
+SET SERVEROUTPUT ON;
+
+DECLARE
+    v_name VARCHAR2(40) := 'John Doe'; -- Example student name
+    v_marks NUMBER := 950;              -- Example marks
+    v_class VARCHAR2(10);               -- Variable to hold the class category
+BEGIN
+    myProcedure(v_name, v_marks, v_class); -- Call the procedure
+
+    DBMS_OUTPUT.PUT_LINE('Student Name: ' || v_name);
+    DBMS_OUTPUT.PUT_LINE('Marks: ' || v_marks);
+    DBMS_OUTPUT.PUT_LINE('Class: ' || v_class); -- Output the class category
+END;
+/
+
+--another procedure pracctice
+create or replace procedure calcMarks(totalScore in number,sname in varchar2,grade out varchar2) as begin 
+if totalScore >= 950 and totalScore <=1500 then
+   grade:='dist';
+   dbms_output.put_line('in dist class');
+elsif totalScore >=850 and totalScore <=950 then
+   grade:='second class';
+   dbms_output.put_line('in second class');
+elsif totalScore>500 and totalScore <=700 then
+   grade:='third class';
+   dbms_output.put_line('in third class');
+else 
+   grade:='no category match';
+   dbms_output.put_line('no category match');
+end if;
+
+end calcMarks;
+/
+
+--make call for procedure
+set serveroutput on;
+
+DECLARE
+ grade varchar(20);
+ name varchar(20):='tommy versetti';
+ obtained int:=912;
+
+begin
+ --take user inout
+
+
+
+ --make procedure call
+ calcMarks(obtained,name,grade);
+ 
+ end;
+
+ /
